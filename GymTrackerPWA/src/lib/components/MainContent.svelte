981d992838f54cd0edb90b155eb2e485db4f463e
MicheleP1004 <michelepanicucci23@gmail.com>
2024-09-18 19:48:10 +0200

    Update MainContent.svelte

--- GymTrackerPWA/src/lib/components/MainContent.svelte
+++ GymTrackerPWA/src/lib/components/MainContent.svelte
@@ -1,13 +1,15 @@
 <script>
-    // Potresti mettere qui la logica per il contenuto principale
+    
 </script>
 
 <style>
-  /* Stile specifico per il contenuto principale */
+  .main-content {
+    background-color: #ffffff;
+    padding: 0px;
+    overflow-y: auto;
+  }
 </style>
 
 <div>
-    <!-- Qui verranno visualizzati i contenuti principali -->
-    <h1>Benvenuto!</h1>
-    <p>Qui puoi interagire con i tuoi contatti.</p>
+    
 </div>
